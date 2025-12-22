323fa2b56d5f4de1a23869406bbd26e7c22db281
Tamar Noselidze <takonoselidze@gmail.com>
2025-12-22 22:17:40 +0100

    adding initial and final health datasets

--- new_disease_data_set/process_data.py
+++ new_disease_data_set/process_data.py
@@ -1,31 +1,23 @@
 import pandas as pd
 
-# -----------------------------
-# Load the data
-# -----------------------------
 df = pd.read_csv("IHME-GBD_2023_DATA-ac7bf47f-1.csv")
 
-# Normalize sex labels for consistency
 df["sex"] = df["sex"].str.lower()
 
-# -----------------------------
 # Create pivot table
-# -----------------------------
 pivot_df = df.pivot_table(
     index=["measure", "location", "year"],
     columns=["cause", "sex"],
     values=["val", "upper", "lower"]
 )
 
-# -----------------------------
+
 # Flatten index into columns
-# -----------------------------
 final_df = pivot_df.reset_index()
 
 # -----------------------------
 # Create clear, consistent column names
 # Format: cause_stat_sex
-# Example: Ischemic_heart_disease_val_male
 # -----------------------------
 clean_columns = []
 
@@ -38,9 +30,6 @@ for col in final_df.columns:
 
 final_df.columns = clean_columns
 
-# -----------------------------
-# Save final output
-# -----------------------------
+
 final_df.to_csv("transformed_gbd_data_2.csv", index=False)
 
-print("Transformation complete. File saved as 'transformed_gbd_data.csv'")
